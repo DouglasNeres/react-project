9a79a0b7b05e13b76634cff8f949516f20e797c5
DouglasNeres <douglasneres06@gmail.com>
2022-08-11 23:59:27 -0300

    react - Props

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
 import HelloWorld from './components/HelloWorld';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
+
 
 function App() {
 
@@ -10,10 +13,10 @@ function App() {
   function sum(x, y) {
       return x + y
   }
-
+ 
   const url = "https://via.placeholder.com/150"
   return (
-    <div className="App">
+    <div className="App">{/* 
       <h1>Hello World in React!</h1>
       <p><u>Dev Web</u></p>
       <h4>My First Project</h4>
@@ -23,8 +26,17 @@ function App() {
       <p>Novo Nome é {NewName}</p>
       <h3>Result Cálc {2+8*2}</h3>
       <h3>{sum(15,5)}</h3>
-      <img src= {url} alt ="My Image"/>
+      <img src= {url} alt ="My Image"/> */}
       <HelloWorld/>
+      <SayMyName nome = "Developer"/>
+      <SayMyName nome = "Enginner"/>
+      <SayMyName nome = {name} />
+      <Pessoa nome = "Neres"
+      idade = "17"
+      profissao = "Dev"
+      foto = "https://via.placeholder.com/150"
+      />
+
     </div>
   );
 }
