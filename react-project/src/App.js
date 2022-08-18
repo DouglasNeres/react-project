38724c0ca3185e6e106d8afb05893f66e2e80df6
DouglasNeres <douglasneres06@gmail.com>
2022-08-18 14:30:15 -0300

    react - Fragments

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -3,6 +3,7 @@ import HelloWorld from './components/HelloWorld';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
 import Frase from './components/Frase';
+import List from './components/List';
 
 
 function App() {
@@ -38,7 +39,7 @@ function App() {
         profissao="Dev"
         foto="https://via.placeholder.com/150"
       />
-
+      <List/>
     </div>
   );
 }
---
+++ react-project/src/components/List.js
@@ -0,0 +1,16 @@
+import Item from './Item'
+
+function List() {
+    return (
+        <>
+            <h1>My List</h1>
+            <ul>
+                <Item marca = "Ferrari"/>
+                <Item marca = "Fiat"/>
+                <Item marca = "Golf"/>
+            </ul>
+        </>
+    )
+}
+
+export default List
