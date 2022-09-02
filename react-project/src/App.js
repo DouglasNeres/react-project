a8972cfdb1c5715e4e4855ce1856fe3244622183
DouglasNeres <douglasneres06@gmail.com>
2022-09-01 23:56:12 -0300

    react - State Lift, First Part

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,15 +1,17 @@
+import {useState} from "react";
 import './App.css';
-import ListaDois from './components/ListaDois';
+import SeuNome from "./components/SeuNome";
 
 function App() {
-
-  const meusItens = ['React', 'Vue', 'Angular']
-
+  /* const meusItens = ['React', 'Vue', 'Angular'] */
+  const [nome, setNome] = useState() 
   return (
     <div className="App">
-      <h1>Renderização de Listas</h1>
-      <ListaDois itens = {meusItens}/>
-      <ListaDois itens = {[]}/>
+      <h1>State Lift</h1>
+      <SeuNome setNome ={setNome}/>
+      {nome}
+{/*       <ListaDois itens = {meusItens}/>
+      <ListaDois itens = {[]}/> */}
     </div>
   );
 }
