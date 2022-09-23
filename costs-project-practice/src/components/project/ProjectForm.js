7682af338973c982a6a773dac670d9a1273a364a
DouglasNeres <douglasneres06@gmail.com>
2022-09-23 00:32:13 -0300

    DevReact - Consumindo API

--- costs-project-practice/src/components/project/ProjectForm.js
+++ costs-project-practice/src/components/project/ProjectForm.js
@@ -1,31 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import Input from '../form/Input'
 import Select from '../form/Select'
 import SubmitButton from '../form/SubmitButton'
 import styles from './ProjectForm.module.css'
 
-function ProjectForm({btnText}) {
-
+function ProjectForm({ btnText }) {
     const [categories, setCategories] = useState([])
-    fetch("http;//localhost:5000/categories", {
-        method: "GET",
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }).then((resp) => resp.json())
-    .then((data) => {
-        setCategories(data)
-    })
-    .catch((err) => console.log(err));
 
+    useEffect(() => {
+        fetch("http://localhost:5000/categories", {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        }).then((resp) => resp.json())
+            .then((data) => {
+                setCategories(data)
+            })
+            .catch((err) => console.log(err));
+
+    }, [])
 
     return (
         <form className={styles.form}>
             <Input type="text" text="Nome do Projeto" name="name" placeholder="Insira o nome do Projeto" />
             <Input type="number" text="Orçamento do Projeto" name="budget" placeholder="Insira o orçamento total" />
             <Select name="category_id" text="Selecione a categoria" options={categories} />
-            <SubmitButton text={btnText}/>
+            <SubmitButton text={btnText} />
         </form>
     )
 }
