48eb268c9b7e417af2df0720b5bdf6dc91786a24
DouglasNeres <douglasneres06@gmail.com>
2022-09-22 23:59:17 -0300

    DevReact - Consumindo API

--- costs-project-practice/src/components/form/Select.js
+++ costs-project-practice/src/components/form/Select.js
@@ -1,11 +1,14 @@
 import styles from './Select.module.css'
 
-function Select({ text, name}) {
+function Select({ text, name, options}) {
     return (
         <div className = {styles.formControl}>
         <label htmlFor={name}>{text}:</label>
         <select name={name} id={name}>
             <option disabled selected>Selecione uma opção</option>
+            {options.map((option) => (
+                <option value={option.id} key={option.id}>{option.name}</option>
+            ))}
         </select>
         </div>
     )
